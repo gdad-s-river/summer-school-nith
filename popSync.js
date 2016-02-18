b57280e3241756aea03c01904e78637b9edf6f0f
gdad-s-river <arihantverma1994@gmail.com>
2016-02-19 04:05:58 +0530

    video added in a modal

--- popSync.js
+++ popSync.js
@@ -1,7 +1,46 @@
 var tl_1 = new TimelineMax();
+
+/* Click event on the Cross button on the Youtube modal*/
+document.querySelector('.fa.fa-times-circle').addEventListener('click', function(){
+	document.querySelector('.info-4').style.display = 'none';
+	showInfo();
+});
+
+document.addEventListener('keyup', function(e){
+	if(e.keyCode === 27){
+		document.querySelector('.info-4').style.display = 'none';
+		showInfo();
+	}
+});
+
+
+/*When the video ends show the text*/
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('youtube-video', {
+    events: {
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+function onPlayerStateChange(event) {
+		if(event.data === 0) {
+				document.querySelector('.info-4').style.display = 'none';
+				showInfo();
+		}
+}
+/* END: When the video ends show the text*/
+
+/*detect the route, if !route = user has hit the root url (else)*/
 if(window.location.hash === "#info"){
 	$( ".pane, .bg, .retrigger, audio" ).remove();
-	showInfo();
+	showVideo();
 }
 else{
 	tl_1.to('.logo', 2, {
@@ -57,28 +96,45 @@ else{
 		onComplete: removeNodes
 	});
 
-	function removeNodes(){
+function removeNodes(){
 		$( ".pane, .bg, .retrigger" ).remove();
 		if(window.location.hash != "#info"){
 			window.location.href = window.location.href + "#info";
+			showVideo();
 		}
 	}
-	showInfo();
+
 }
 
+function showVideo(){
+	document.querySelector('body').style.background = "transparent";
+	tl_1.to(".info-4", 1, {
+		display: "block",
+	});
+
+};
+
 function showInfo(){
+
 	tl_1.to("#viewport", 1, {
 		display: "block",
 	});
 
 	tl_1.to(".info-1, .info-2", 1.5, {
 		display: "block",
-		top: 0,	
+		top: 0,
 		ease: Elastic.easeOut.config(1, 0.3)
 	});
+
+	tl_1.to(".info-links", 0.5, {
+		display: "block",
+		top: "50%",
+		ease: Elastic.easeOut.config(1, 0.3)
+	});
+
 	tl_1.to(".info-3", 1, {
 		display: "block",
-		bottom: "0",	
+		bottom: "-6%",
 		ease: Elastic.easeOut.config(1, 0.3)
 	});
 	tl_1.to(".author", 1, {
